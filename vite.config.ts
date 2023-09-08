209ca8e190b2e815201f77e5f2137c943ea98729
Andersen <outuser51@cathaysec.com.tw>
2023-09-08 17:08:15 +0800

    fix(build): adjust vite config

--- vite.config.ts
+++ vite.config.ts
@@ -13,9 +13,11 @@ import VueDevTools from 'vite-plugin-vue-devtools';
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 import UnoCss from 'unocss/vite';
 
+const repoName = 'actspace';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/actspace/',
+  base: `/${repoName}/`,
   server: {
     host: 'localhost',
     port: 8888,
@@ -87,17 +89,17 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            src: '/pwa-192x192.png',
+            src: `/${repoName}/pwa-192x192.png`,
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: `/${repoName}/pwa-512x512.png`,
             sizes: '512x512',
             type: 'image/png',
           },
           {
-            src: '/pwa-512x512.png',
+            src: `/${repoName}/pwa-512x512.png`,
             sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable',
