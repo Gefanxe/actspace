666f27ca438d874dea15319bf330b81d7a0fc6db
Gefanxe <actfire@gmail.com>
2023-09-19 08:38:58 +0000

    deploy to Github pages

---
+++ assets/index-b1805516.js
@@ -0,0 +1,1 @@
+import{d as t,r as a,c as n,a as o,w as r,o as c,b as s}from"./index-17d26520.js";const m=t({name:"Home",__name:"index",setup(p){return(l,_)=>{const e=a("RouterLink");return c(),n("div",null,[o(e,{to:"/reference","aria-current":"page","aria-label":"Reference",class:"btn-ghost btn px-2 text-[20px]"},{default:r(()=>[s(" 參考資料 ")]),_:1})])}}});export{m as default};
